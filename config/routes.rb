8a9bee6f0e9d2fd9076641d0394c0d8fbb10fdd1
megumi-68 <megu.megu.megu68@gmail.com>
2022-02-21 17:05:53 +0900

    ユーザーページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+
+  resources :users, only: [:index, :edit, :update]
+  get 'users/account' => 'users#account', as: 'account'
+  get 'users/profile' => 'users#profile', as: 'profile'
   devise_for :users,controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
@@ -6,6 +11,10 @@ Rails.application.routes.draw do
   
   root to: 'toppages#index'
   
-  resources :rooms
+  get 'rooms/posts' => 'rooms#posts', as: 'posts'
+  resources :rooms, only: [:new, :index, :show, :create, :destroy, :update, :edit]
+
+  resources :reservations, only: [:new, :index, :show, :create, :destroy]
+  post 'reservations/confirm' => 'reservations#confirm', as: 'confirm'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
