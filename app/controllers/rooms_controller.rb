0181e5c020d755107cc7c305826cebb23f407d27
megumi-68 <megu.megu.megu68@gmail.com>
2022-02-21 14:38:00 +0900

    コントローラアクション実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,58 @@
 class RoomsController < ApplicationController
+
+    def index
+        @rooms = Room.all.order(updated_at: 'ASC').page(params[:page]).per(8)
+      end
+      
+      def new
+        @room = Room.new
+      end
+      
+      def edit
+        @room = Room.find(params[:id])
+      end
+      
+      def create
+        @room = Room.new(room_params)
+        @room.user = current_user
+        if @room.save
+          flash[:notice] = "部屋を新規登録しました"
+          redirect_to room_path(@room)
+        else
+          render "new"
+        end
+      end
+      
+      def update
+        @room = Room.find(params[:id])
+        @room.update(room_params)
+        @room.user = current_user
+        if @room.save
+          flash[:notice] = "部屋情報を更新しました"
+          redirect_to rooms_path
+        else
+          render "edit"
+        end
+      end
+      
+      def posts
+        @rooms = current_user.rooms.order(updated_at: 'ASC').page(params[:page]).per(4)
+      end
+      
+      
+      def show
+        @room = Room.find(params[:id])
+        @reservation = Reservation.new
+      end
+      
+      def destroy
+        room = Room.find(params[:id])
+        room.user_id = current_user.id
+        room.destroy
+        redirect_back(fallback_location: root_path)
+      end
+      
+      def room_params
+        params.require(:room).permit(:name, :introduce, :image, :address, :price, :user_id)
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+       end
+       
+       def account
+        @user = current_user
+       end
+       
+       def profile
+        @user = current_user
+       end
+       
+       def edit
+        @user = current_user
+       end
+       
+       def update
+        @user = current_user
+        @user.update(user_params)
+        @user.update(image: nil) if params[:image_delete].present?
+        if @user.update(user_params)
+         flash[:notice] = "ユーザー情報を更新しました"
+         redirect_to profile_path
+        else
+         redirect_to root_path
+        end
+       end
+       
+       def user_params
+        params.require(:user).permit(:name, :email, :introduce, :image, :encrypted_password)
+       end
 end
